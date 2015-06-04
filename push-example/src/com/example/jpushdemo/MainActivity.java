b0939fb5c57315a885e6bcd6233b7d20764f2456
pangjianhai <821761509@qq.com>
2015-06-04 21:51:59 +0800

    c

--- push-example/src/com/example/jpushdemo/MainActivity.java
+++ push-example/src/com/example/jpushdemo/MainActivity.java
@@ -34,68 +34,10 @@ public class MainActivity extends InstrumentedActivity implements
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		initView();
 		registerMessageReceiver(); // used for receive msg
 	}
 
-	private void initView() {
-		TextView mImei = (TextView) findViewById(R.id.tv_imei);
-		String udid = ExampleUtil.getImei(getApplicationContext(), "");
-		if (null != udid)
-			mImei.setText("IMEI: " + udid);
-
-		TextView mAppKey = (TextView) findViewById(R.id.tv_appkey);
-		String appKey = ExampleUtil.getAppKey(getApplicationContext());
-		if (null == appKey)
-			appKey = "AppKey异常";
-		mAppKey.setText("AppKey: " + appKey);
-
-		String packageName = getPackageName();
-		TextView mPackage = (TextView) findViewById(R.id.tv_package);
-		mPackage.setText("PackageName: " + packageName);
-
-		String versionName = ExampleUtil.GetVersion(getApplicationContext());
-		TextView mVersion = (TextView) findViewById(R.id.tv_version);
-		mVersion.setText("Version: " + versionName);
-
-		mInit = (Button) findViewById(R.id.init);
-		mInit.setOnClickListener(this);
-
-		mStopPush = (Button) findViewById(R.id.stopPush);
-		mStopPush.setOnClickListener(this);
-
-		mResumePush = (Button) findViewById(R.id.resumePush);
-		mResumePush.setOnClickListener(this);
-
-		mSetting = (Button) findViewById(R.id.setting);
-		mSetting.setOnClickListener(this);
-
-		msgText = (EditText) findViewById(R.id.msg_rec);
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.init:
-			init();
-			break;
-		case R.id.setting:
-			Intent intent = new Intent(MainActivity.this, PushSetActivity.class);
-			startActivity(intent);
-			break;
-		case R.id.stopPush:
-			JPushInterface.stopPush(getApplicationContext());
-			break;
-		case R.id.resumePush:
-			JPushInterface.resumePush(getApplicationContext());
-			break;
-		}
-	}
-
-	// 初始化 JPush。如果已经初始化，但没有登录成功，则执行重新登录。
-	private void init() {
-		JPushInterface.init(getApplicationContext());
-	}
+	
 
 	@Override
 	protected void onResume() {
@@ -153,6 +95,12 @@ public class MainActivity extends InstrumentedActivity implements
 			msgText.setText(msg);
 			msgText.setVisibility(android.view.View.VISIBLE);
 		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
