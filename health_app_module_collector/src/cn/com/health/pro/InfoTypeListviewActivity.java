b79fda3399ec853bef3f0bf0b0fbd37ef1d28cc4
pangjianhai <821761509@qq.com>
2015-06-14 12:05:34 +0800

    submit

--- health_app_module_collector/src/cn/com/health/pro/InfoTypeListviewActivity.java
+++ health_app_module_collector/src/cn/com/health/pro/InfoTypeListviewActivity.java
@@ -99,7 +99,6 @@ public class InfoTypeListviewActivity extends BaseActivity {
 			@Override
 			public void onSuccess(ResponseInfo<String> responseInfo) {
 				String rs = responseInfo.result;
-				System.out.println("rs：" + rs);
 				List<InfoTypeEntity> lst = ModelUtil.parseInfo(rs);
 				infoList.addAll(lst);
 				adapter.notifyDataSetChanged();
