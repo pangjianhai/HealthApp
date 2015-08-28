489fe4b18e42ecb4ad31e421dc18b8540d9d9ea5
pangjianhai <821761509@qq.com>
2015-08-28 12:02:04 +0800

    push

--- health_app_module_collector/src/cn/com/hzzc/health/pro/receiver/MyReceiver.java
+++ health_app_module_collector/src/cn/com/hzzc/health/pro/receiver/MyReceiver.java
@@ -28,45 +28,43 @@ public class MyReceiver extends BroadcastReceiver {
 		System.out.println("[MyReceiver] onReceive - " + intent.getAction()
 				+ ", extras: " + printBundle(bundle));
 		if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+			System.out
+					.println("============================JPushInterface.ACTION_REGISTRATION_ID");
 			String regId = bundle
 					.getString(JPushInterface.EXTRA_REGISTRATION_ID);
 
 		} else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent
-				.getAction())) {
+				.getAction())) {// 接收自定义消息
+			System.out
+					.println("============================JPushInterface.ACTION_MESSAGE_RECEIVED");
 			processCustomMessage(context, bundle);
 
 		} else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent
-				.getAction())) {
-			System.out.println("[MyReceiver] 接收到推送下来的通知");
+				.getAction())) {// 获取消息通知的时候进入此分支
+			System.out
+					.println("============================JPushInterface.ACTION_NOTIFICATION_RECEIVED");
 			int notifactionId = bundle
 					.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
 
 		} else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent
-				.getAction())) {
-
+				.getAction())) {// 打开消息的时候进入此分支
+			System.out
+					.println("============================JPushInterface.ACTION_NOTIFICATION_OPENED");
 			String str = bundle.getString(EXTRAS_KEY);
-			try {
-				JSONObject or_obj = new JSONObject(str);
-				String zcId = or_obj.getString("zcId");
-				String zcTitle = or_obj.getString("zcTitle");
-				Intent it = new Intent(context, InfoDetailActivity.class);
-				it.putExtra("doc_id", zcId);
-				it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-						| Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				context.startActivity(it);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
 		} else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent
 				.getAction())) {
-
+			System.out
+					.println("============================JPushInterface.ACTION_RICHPUSH_CALLBACK");
 		} else if (JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent
 				.getAction())) {
+			System.out
+					.println("============================JPushInterface.ACTION_CONNECTION_CHANGE");
 			boolean connected = intent.getBooleanExtra(
 					JPushInterface.EXTRA_CONNECTION_CHANGE, false);
 		} else {
+			System.out
+					.println("============================JPushInterface.otheras");
 			System.out.println("[MyReceiver] Unhandled intent - "
 					+ intent.getAction());
 		}
@@ -89,6 +87,6 @@ public class MyReceiver extends BroadcastReceiver {
 
 	// send msg to MainActivity
 	private void processCustomMessage(Context context, Bundle bundle) {
-		
+
 	}
 }
